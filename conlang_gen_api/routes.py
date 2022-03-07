c2727f1a22c1638ca80ed356b78d63881fd3d39d
effervescent000 <kislio01@gmail.com>
2022-03-07 11:36:13 -0500

    added removal of phonemes based on missing features

--- conlang_gen_api/routes.py
+++ conlang_gen_api/routes.py
@@ -15,7 +15,7 @@ def generate_new():
                 phonemes[k] = {}
             phonemes[k][label] = v
 
-    def get_phonemes_with_voicing_contrast(condition_label, condition_value, features):
+    def get_phonemes_with_features(condition_label, condition_value, features):
         if features["voicing_contrast"] == "both":
             return model[model[condition_label] == condition_value]
         elif features["voicing_contrast"] == "none":
@@ -34,6 +34,32 @@ def generate_new():
                     model[(model[condition_label] == condition_value) & (model["cnt"] == "-") & (model["vce"] == "-")],
                 ]
             )
+
+    def clean_phonemes(phonemes, features):
+        def remove_nasals():
+            remove_list = []
+            for key, values in phonemes.items():
+                if values["nas"] == "+" and values["con"] == "+":
+                    remove_list.append(key)
+            for phone in remove_list:
+                phonemes.pop(phone)
+
+        def remove_fricatives():
+            remove_list = []
+            for key, values in phonemes.items():
+                if values["con"] == "+" and values["son"] == "-" and values["cnt"] == "+":
+                    remove_list.append(key)
+            for phone in remove_list:
+                phonemes.pop(phone)
+
+        if features["missing_consonants"] != "none":
+            if features["missing_consonants"] == "fricatives_and_nasals":
+                remove_nasals()
+                remove_fricatives()
+            elif features["missing_consonants"] == "fricatives":
+                remove_fricatives()
+            elif features["missing_consonants"] == "nasals":
+                remove_nasals()
 
     phonemes = {}
 
@@ -64,26 +90,33 @@ def generate_new():
         # add labials
         # get rows from model with +lab
         # add the symbol to phonemes list
-        labials = get_phonemes_with_voicing_contrast("lab", "+", features)
+        labials = get_phonemes_with_features("lab", "+", features)
 
         for column_name, phoneme in labials.items():
             # print(column_name, phoneme.to_dict())
             create_articulation_dict(column_name, phoneme.to_dict(), phonemes)
-    # generate alveolars (make this choose between dentals and alveolars, or both?)
+    # generate alveolars or dentals (choosing between, with a small chance to have both)
     r = random()
     coronals = None
     if r < 0.45:
-        coronals = get_phonemes_with_voicing_contrast("cor", 2, features)
+        coronals = get_phonemes_with_features("cor", 2, features)
     elif r < 0.9:
-        coronals = get_phonemes_with_voicing_contrast("cor", 1, features)
+        coronals = get_phonemes_with_features("cor", 1, features)
     else:
         coronals = pd.concat(
             [
-                get_phonemes_with_voicing_contrast("cor", 2, features),
-                get_phonemes_with_voicing_contrast("cor", 1, features),
+                get_phonemes_with_features("cor", 2, features),
+                get_phonemes_with_features("cor", 1, features),
             ]
         )
     for column_name, phoneme in coronals.items():
         create_articulation_dict(column_name, phoneme.to_dict(), phonemes)
+    # palatals
+    r = random()
+    if r < 0.2:
+        palatals = get_phonemes_with_features("cor", 4, features)
+        for column_name, phoneme in palatals.items():
+            create_articulation_dict(column_name, phoneme.to_dict(), phonemes)
 
+    clean_phonemes(phonemes, features)
     return jsonify(phonemes)
